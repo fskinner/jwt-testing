6b32f2d8e659f8a53f742d27bcaf707e8399e893
Felipe Skinner <felipeskinner@gmail.com>
2015-10-19 13:40:37 -0200

    Add Get API

--- server.js
+++ server.js
@@ -33,18 +33,17 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
-
-app.get('/setup', function(req, res) {
+app.get('/setup', (req, res) => {
 
   // create a sample user
-  var nick = new User({
-    name: 'Nick Cerminara',
-    password: 'password',
+  const sampleUser = new User({
+    name: 'John Tester',
+    password: 'passwordz',
     admin: true
   });
 
   // save the sample user
-  nick.save(function(err) {
+  sampleUser.save((err) => {
     if (err) throw err;
 
     console.log('User saved successfully');
@@ -53,7 +52,27 @@ app.get('/setup', function(req, res) {
 });
 
 // API ROUTES -------------------
-// we'll get
+// get an instance of the router for api routes
+const apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', (req, res) => {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 // =======================
 // start the server ======
